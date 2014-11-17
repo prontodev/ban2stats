99daa9929cdf132eae58eea9f7c071fc8f3b0d83
Chanita Anuwong <chanita@odd-e.com>
2014-11-17 16:40:03 +0700

    - test invalid ip case

--- attack/tests/test_recorder.py
+++ attack/tests/test_recorder.py
@@ -12,3 +12,8 @@ class TestRecorder(TestCase):
         geo_details = self.attack_recorder.get_geo_data(ip=ip)
         self.assertEqual(self.attack_recorder.data['country'], u'US')
         self.assertEqual(self.attack_recorder.data['geo_location'], u'CA, United States')
+
+    def test_get_geo_data_invalid_ip(self):
+        ip = '127.8.8.8'
+        self.assertRaisesMessage(ValueError, 'Cannot find Geo details for this IP 127.8.8.8',
+                                 self.attack_recorder.get_geo_data, ip=ip )
