5da2030799bc25623cfeccecd2e25a1fb2c92ec5
Chanita Anuwong <chanita@odd-e.com>
2014-12-04 17:56:52 +0700

    - refactor Attack model to use Point / Location

--- attack/models.py
+++ attack/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.gis.geos import Point
 
 
 class Attack(models.Model):
@@ -9,8 +10,12 @@ class Attack(models.Model):
     protocol = models.CharField(max_length=140)
     port = models.CharField(max_length=140)
 
-    longitude = models.CharField(max_length=30)
-    latitude = models.CharField(max_length=30)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     country_code = models.CharField(max_length=3)
     geo_location = models.CharField(max_length=140)
     timestamp = models.DateTimeField()
+
+    def get_location(self):
+        # Remember, longitude FIRST!
+        return Point(self.longitude, self.latitude)
