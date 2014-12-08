c4bfbf1ffdea3cd4e3cf214de044c2c48005de4c
Chanita Anuwong <chanita@odd-e.com>
2014-12-08 17:37:00 +0700

    - remove unused function

--- stats/packages/blocked_location.py
+++ stats/packages/blocked_location.py
@@ -14,24 +14,9 @@ class BlockedLocationPackageBuilderMinimized(PackageBuilder):
         self.objects = blocked_ip_list
         return blocked_ip_list
 
-    def format_last_seen_string(self, last_seen_raw):
-        if last_seen_raw is None:
-            return 'n/a'
-        last_seen_datetime = parse(last_seen_raw)
-        return last_seen_datetime.strftime("%b %d, %Y %H:%M:%S %z")
-
     def render_each_object(self, object):
         latitude, longitude = eval(object[0])
         data = {'latitude':latitude, 'longitude':longitude, 'count':object[1]}
         template = '''["{latitude}","{longitude}",{count}]'''
         output_string = template.format(**data)
         return output_string
-
-    def render_as_javascript(self):
-        template = """
-        "blocked_ips": {0},\n
-        "blocked_ip_count": "{1}"
-        """
-        return template.format(self.render_all_objects_as_list(), self.objects_count_as_string(len(self.objects)))
-
-
