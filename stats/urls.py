1d8674c9c1515b58bf440ed6d923df80595d4234
Chanita Anuwong <chanita@odd-e.com>
2014-12-09 11:42:19 +0700

    - remove unused line

--- stats/urls.py
+++ stats/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^attacked_services/$', 'stats.views.get_attacked_services', name='get_attacked_services'),
     url(r'^blocked_countries/$', 'stats.views.get_blocked_countries', name='get_blocked_countries'),
-    # url(r'^blocked_ips/$', 'stats.views.get_blocked_ips', name='get_blocked_ips'),
     url(r'^blocked_locations/$', 'stats.views.get_block_locations', name='get_blocked_locations'),
     url(r'^location_details/$', 'stats.views.get_location_details', name='get_location_details'),
 )
