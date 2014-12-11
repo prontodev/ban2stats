58e4f5b60abc92ee4df8ac4980b17c1a5e879b1a
Chanita Anuwong <chanita@odd-e.com>
2014-12-11 14:15:09 +0700

    - script to generate attacks

--- attack/management/commands/generate_attacks.py
+++ attack/management/commands/generate_attacks.py
@@ -22,68 +22,68 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        for i in range(172,173):
-            ip = '114.82.{0}.3'.format(i)
-            attack(ip, 'Evil Service')
-
-        for i in range(2,220):
-            ip = '114.{0}.19.3'.format(i)
-            attack(ip, 'Company Portal')
-
-        for i in range(33, 80):
-            ip = '114.48.19.{0}'.format(i)
-            attack(ip, 'Human Resource Control Portal')
-
-        for i in range(20, 233):
-            ip = '114.48.20.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(40, 233):
-            ip = '114.48.20.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(83, 133):
-            ip = '114.48.20.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(12, 140):
-            ip = '114.109.188.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(32, 109):
-            ip = '72.14.20.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(2, 179):
-            ip = '72.14.12.{0}'.format( i)
-            attack(ip, 'Human Resource Control Portal')
-
-        for i in range(12, 240):
-            ip = '114.109.{0}.4'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(32, 109):
-            ip = '72.90.20.{0}'.format( i)
-            attack(ip, 'Financial Department Portal')
-        for i in range(2, 179):
-            ip = '72.14.1.{0}'.format(i)
-            attack(ip, 'Human Resource Control Portal')
-
-        call_command('rebuild_index', interactive=False, verbosity=0)
-
-        for i in range(172,210):
-            for j in range(2,220):
-                for k in range(2,220):
-                    for l in range(1, 248):
-                        ip = '{0}.{1}.{2}.{3}'.format(i,j,k,l)
-                        attack(ip, 'Evil Service')
-
-                    ip = '114.{0}.{1}.{2}'.format(i,j,k)
-                    attack(ip, 'Company Portal')
-
-                ip = '{3}.{1}.{2}.{0}'.format(i,j,k)
-                attack(ip, 'Human Resource Control Portal')
-
-                ip = '114.{j}.20.{0}'.format(i,j)
-                attack(ip, 'Financial Department Portal')
-
-            for j in range(40, 233):
-                ip = '114.48.{1}.{0}'.format(i,j)
-                attack(ip, 'Financial Department Portal')
+        # for i in range(172,173):
+        #     ip = '114.82.{0}.3'.format(i)
+        #     attack(ip, 'Evil Service')
+        #
+        # for i in range(2,220):
+        #     ip = '114.{0}.19.3'.format(i)
+        #     attack(ip, 'Company Portal')
+        #
+        # for i in range(33, 80):
+        #     ip = '114.48.19.{0}'.format(i)
+        #     attack(ip, 'Human Resource Control Portal')
+        #
+        # for i in range(20, 233):
+        #     ip = '114.48.20.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(40, 233):
+        #     ip = '114.48.20.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(83, 133):
+        #     ip = '114.48.20.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(12, 140):
+        #     ip = '114.109.188.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(32, 109):
+        #     ip = '72.14.20.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(2, 179):
+        #     ip = '72.14.12.{0}'.format( i)
+        #     attack(ip, 'Human Resource Control Portal')
+        #
+        # for i in range(12, 240):
+        #     ip = '114.109.{0}.4'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(32, 109):
+        #     ip = '72.90.20.{0}'.format( i)
+        #     attack(ip, 'Financial Department Portal')
+        # for i in range(2, 179):
+        #     ip = '72.14.1.{0}'.format(i)
+        #     attack(ip, 'Human Resource Control Portal')
+        #
+        # call_command('rebuild_index', interactive=False, verbosity=0)
+        #
+        # for i in range(172,210):
+        #     for j in range(2,220):
+        #         for k in range(2,220):
+        #             for l in range(1, 248):
+        #                 ip = '{0}.{1}.{2}.{3}'.format(i, j, k, l)
+        #                 attack(ip, 'Evil Service')
+        #
+        #             ip = '114.{0}.{1}.{2}'.format(i, j, k)
+        #             attack(ip, 'Company Portal')
+        #
+        #         ip = '{3}.{1}.{2}.{0}'.format(i, j, k, l)
+        #         attack(ip, 'Human Resource Control Portal')
+        #
+        #         ip = '114.{j}.20.{0}'.format(i,j)
+        #         attack(ip, 'Financial Department Portal')
+        #
+        #     for j in range(40, 233):
+        #         ip = '114.48.{1}.{0}'.format(i,j)
+        #         attack(ip, 'Financial Department Portal')
 
         for i in range(83, 133):
             ip = '114.48.20.{0}'.format( i)
